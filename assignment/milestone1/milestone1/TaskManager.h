7a55f3ddbfbaf3c3254bb609dd64e5cf81700e9a
sorash <sorash@sorash-laptop>
2016-07-15 01:16:56 -0700

    changed some stuff

---
+++ assignment/milestone1/milestone1/TaskManager.h
@@ -0,0 +1,16 @@
+#ifndef TASK_MANAGER_H_
+#define TASK_MANAGER_H_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <vector>
+#include "Task.h"
+
+class TaskManager
+{
+	void read(std::string& file);
+};
+
+#endif
--- assignment/milestone1/milestone1/Utilities.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILITIEs_H_
-#define UTILITIEs_H_
-
-class Utilities
-{
-
-};
-
-#endif
