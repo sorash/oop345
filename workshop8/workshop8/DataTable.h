c53d9b798969e0567cd1e4dc160465276047a08e
unknown <sashrafi3@SY-T4044-W836.SeneDS.senecacollege.ca>
2016-07-19 22:20:49 -0400

    hey something works!!!

--- workshop8/workshop8/DataTable.h
+++ workshop8/workshop8/DataTable.h
@@ -4,25 +4,72 @@
 #include <fstream>
 #include <vector>
 #include <utility>
+#include <sstream>
 
 namespace w8
 {
-	template<typename T>
+	template<class T>
 	class DataTable
 	{
 		std::vector<std::pair<T, T>> points;
 
 	public:
-		DataTable(std::ifstream& is, int width, int decimals);
-		T mean() const;
-		T sigma() const;
-		T median() const;
-		void regression(T& slope, T& y_intercept) const;
-		void display(std::ostream& os) const;
+		DataTable(std::ifstream& is, int width, int decimals)
+		{
+			// read line
+			std::string line;
+			std::getline(is, line);
+
+			// insert the line into a stringstream
+			std::stringstream ss;
+
+			// if line is not blank, return a respective product, else return nullptr
+			if (!line.empty())
+			{
+				ss.str(line);
+				T first, second;
+				ss >> first >> second;
+				std::pair<T, T> temp;
+				temp.first = first;
+				temp.second = second;
+
+				// add to points
+				points.push_back(temp);
+			}
+		}
+
+		T mean() const
+		{
+			return 0;
+		}
+
+		T sigma() const
+		{
+			return 1;
+		}
+
+		T median() const
+		{
+			return 2;
+		}
+
+		void regression(T& slope, T& y_intercept) const
+		{
+
+		}
+
+		void display(std::ostream& os) const
+		{
+
+		}
 	};
 
 	template<class T>
-	std::ostream& operator<<(std::ostream& os, const DataTable<T>& dt);
+	std::ostream& operator<<(std::ostream& os, const DataTable<T>& dt)
+	{
+		dt.display(os);
+		return os;
+	}
 }
 
 #endif
