aa085bdc2b7e9d1c11c55cbf740bf4ac3e5c83d1
sorash <sorash@sorash-laptop>
2016-07-26 11:07:05 -0700

    modified ms2 for reading all files together

--- assignment/milestone2/milestone2/milestone_2.cpp
+++ assignment/milestone2/milestone2/milestone_2.cpp
@@ -3,14 +3,18 @@
 
 int main(int argc, char** argv)
 {
-	if (argc != 3) {
+	if (argc != 7) {
 		std::cerr << "*** invalid number of arguments ***\n"
-			<< "Usage: " << "[1]tasks file [2]delimiter" << std::endl;
+			<< "Usage: " << "[1]tasks file [2]tasks delimiter [3]items file [4]items delimiter [5]orders file [6]orders delimiter" << std::endl;
 		return 1;
 	}
 	std::cout << "Command Line Arguments" << std::endl << "----------------------" << std::endl;
 	std::cout << "Tasks File Specified          = " << argv[1] << std::endl;
-	std::cout << "File Record Field Delimiter   = " << argv[2][0] << std::endl << std::endl;
+	std::cout << "Task Record Field Delimiter   = " << argv[2][0] << std::endl << std::endl;
+	std::cout << "Items File Specified          = " << argv[3] << std::endl;
+	std::cout << "Items Record Field Delimiter   = " << argv[4][0] << std::endl << std::endl;
+	std::cout << "Orders File Specified          = " << argv[5] << std::endl;
+	std::cout << "Orders Record Field Delimiter   = " << argv[6][0] << std::endl << std::endl;
 
 	// create a new item manager
 	ItemManager im = ItemManager(std::string(argv[1]), argv[2][0]);
