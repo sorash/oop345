841de360d35349455ce7a58342808d59f2ec3a1c
unknown <sashrafi3@SY-T3073-W835.SeneDS.senecacollege.ca>
2016-08-09 22:26:43 -0400

    finished finding source node

--- assignment/milestone3/milestone3/Factory.cpp
+++ assignment/milestone3/milestone3/Factory.cpp
@@ -18,8 +18,22 @@ void Factory::findSource(TaskManager& tm)
 	Task* source = nullptr;
 	std::vector<Task> tasks = tm.getTasks();
 
-
-
-	if(source != nullptr)
-		std::cout << source->getName() << std::endl;
+	for (auto task : tasks)
+	{
+		// check if task has an accept but not a reject
+		if (task.hasAccept() && !task.hasReject())
+		{
+			// check if task name is not any other task's accept or reject slot
+			int i = 0;
+			for (; i < tasks.size(); i++)
+			{
+				if (task.getName() == tasks[i].getAccept() || task.getName() == tasks[i].getReject())
+					break;
+			}
+			
+			// if task is not any other task's accept or reject slot, make it the source node
+			if (i == tasks.size())
+				source = &task;
+		}
+	}
 }
